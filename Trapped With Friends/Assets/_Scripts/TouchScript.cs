086fbe887867d5927ca6a171e21c0d79303e9652
Calvin Kopitz <calvinjk@umich.edu>
2017-01-23 11:17:48 -0500

    Moved camera rotation to buttons and to be more robust on different devices

--- Trapped With Friends/Assets/_Scripts/TouchScript.cs
+++ Trapped With Friends/Assets/_Scripts/TouchScript.cs
@@ -3,20 +3,16 @@ using System.Collections;
 
 public class TouchScript : MonoBehaviour {
     //Initialize
+    public float cameraRoatationSpeed = 20;
+    public bool ____________________________;
     bool rotating = false;
 
 	void Update () {
 
-	    if (Input.touches[0].position.x < 300 && !rotating) { //Rotate left
-            StartCoroutine(RotateCamera(Vector3.down * 90, 1));
-        }
-
-        if (Input.touches[0].position.x > 500 && !rotating) { //Rotate right
-            StartCoroutine(RotateCamera(Vector3.up * 90, 1));
-        }
     }
 
-    void OnGUI() { //Everything in here is currently just to tell if fingers are touching the screen
+    void OnGUI() {
+        // Used for development to see if fingers are touching the screem
         foreach(Touch touch in Input.touches) {
             string message = "";
             message += "ID: " + touch.fingerId + "\n";
@@ -28,18 +24,29 @@ public class TouchScript : MonoBehaviour {
             int num = touch.fingerId;
             GUI.Label(new Rect(0 + 130 * num, 0, 120, 100), message);
         }
+
+        // Rotate camera if GUI buttons are pressed
+        if (GUI.Button(new Rect(0, 0, Screen.width / 4, Screen.height), "") && !rotating) {
+            StartCoroutine(RotateCamera(Vector3.down * 90, cameraRoatationSpeed));
+        }
+        if (GUI.Button(new Rect(Screen.width / 4 * 3, 0, Screen.width / 4, Screen.height), "") && !rotating) {
+            StartCoroutine(RotateCamera(Vector3.up * 90, cameraRoatationSpeed));
+        }
     }
 
+    // Coroutine for rotating the camera given an angle to rotate and time to do it in
     IEnumerator RotateCamera(Vector3 byAngle, float inTime) {
         var fromAngle = transform.rotation;
         var toAngle = Quaternion.Euler(transform.eulerAngles + byAngle);
         rotating = true;
 
-        for (var t = 0f; t < 1; t += 0.05f / inTime) {
+        // Actual work for the coroutine is done here in moving the camera
+        for (var t = 0f; t < 1; t += 1f / inTime) {
             transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
             yield return null;
         }
 
+        // Once rotation is "done", make sure it snaps to the nearest 90 degrees
         var vec = transform.eulerAngles;
         vec.x = Mathf.Round(vec.x / 90) * 90;
         vec.y = Mathf.Round(vec.y / 90) * 90;
