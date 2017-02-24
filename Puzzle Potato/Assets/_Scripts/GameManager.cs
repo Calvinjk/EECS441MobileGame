870bc5293a5cd284b4cad1ec50fca87d573c961f
Calvin Kopitz <calvinjk@umich.edu>
2017-02-24 14:32:13 -0500

    Added comments into existing code for clarification

--- Puzzle Potato/Assets/_Scripts/GameManager.cs
+++ Puzzle Potato/Assets/_Scripts/GameManager.cs
@@ -6,8 +6,9 @@ namespace com.aaronandco.puzzlepotato {
     public class GameManager : MonoBehaviour {
 
 
-        public bool ____________________________;  //Separation between public and "private" variables in the inspector
+        public bool ____________________________;  // Separation between public and "private" variables in the inspector
 
+        // This is the main player list that will be in effect for the duration of the game
         public List<string> players = new List<string>();
 
         void Awake() {
--- Puzzle Potato/Assets/_Scripts/SetupController.cs
+++ Puzzle Potato/Assets/_Scripts/SetupController.cs
@@ -7,22 +7,25 @@ using System.Collections.Generic;
 
 namespace com.aaronandco.puzzlepotato {
     public class SetupController : MonoBehaviour {
+        // All these must be added by hand in the inspector
         public GameObject addPlayersPanel;
         public GameObject titlePanel;
         public InputField playerNameInput;
         public GameObject playerCount;
         public GameObject playerList;
 
-        public bool ____________________________;  //Separation between public and "private" variables in the inspector
+        public bool ____________________________;  // Separation between public and "private" variables in the inspector
 
         public List<string> playerNames = new List<string>();
         public GameManager gameManagerScript;
 
+        // Make sure the players only see the title screen at first and find the GameManager script
         void Awake() {
             addPlayersPanel.SetActive(false);
             gameManagerScript = (GameManager)GameObject.Find("GameManager").GetComponent("GameManager");
         }
 
+        // Toggles the view between title screen and player select
         public void ToggleTitleView() {
             if (addPlayersPanel.activeInHierarchy == true) {
                 addPlayersPanel.SetActive(false);
@@ -33,21 +36,24 @@ namespace com.aaronandco.puzzlepotato {
             }
         }
 
+        // Adds a new player to the list, updates on screen information to reflect this
         public void AddPlayerToList() {
             playerNames.Add(playerNameInput.text);
             Text playerListText = playerList.GetComponent<Text>();
 
             playerCount.GetComponent<Text>().text = "Current Player Count: " + playerNames.Count;
-            playerListText.text = "Current Players:\n";
+            playerListText.text = "Current Players:";
 
             for (int i = 0; i < playerNames.Count; ++i) {
-                playerListText.text = playerListText.text + playerNames[i] + "\n";
+                playerListText.text = "\n" + playerListText.text + playerNames[i];
             }
 
             playerNameInput.text = "";
             playerListText.color = Color.white;
         }
 
+        // Write the current player list to the master GameManager and load the next scene
+        // TODO -- Checks for minimum players and such
         public void BeginGame() {
             gameManagerScript.players = playerNames;
             SceneManager.LoadScene(1);
