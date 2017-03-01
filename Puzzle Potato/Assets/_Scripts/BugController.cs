0e99372c623268268402ad20a821d04c73969188
Calvin Kopitz <calvinjk@umich.edu>
2017-03-01 05:57:17 -0500

    Minor addition to BugController

--- Puzzle Potato/Assets/_Scripts/BugController.cs
+++ Puzzle Potato/Assets/_Scripts/BugController.cs
@@ -4,6 +4,10 @@ using System.Collections;
 namespace com.aaronandco.puzzlepotato {
     public class BugController : MonoBehaviour {
         public float speed = 2f;
-        
+
+        public bool ____________________________;  // Separation between public and "private" variables in the inspector
+
+
+
     }
 }
