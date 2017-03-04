458d5a600e935bd9b6b25e3a57dab59efaf78209
Calvin Kopitz <calvinjk@umich.edu>
2017-03-04 08:15:14 -0500

    I believe this fixes #31, UI scalings.  Crashing issue fixed

--- Puzzle Potato/Assets/_Scripts/BugController.cs
+++ Puzzle Potato/Assets/_Scripts/BugController.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 namespace com.aaronandco.puzzlepotato {
     public class BugController : MonoBehaviour {
-        public float speed = 2f;
+        public float speed = 2.1f;
 
         public bool ____________________________;  // Separation between public and "private" variables in the inspector
 
+        public float topBound = 5f;
+        public float botBound = -5f;
+        public float rightBound = 7.5f;
+        public float leftBound = -7.5f;
 
+        void FixedUpdate() {
+            transform.position += transform.forward * speed;
+        }
 
     }
 }
