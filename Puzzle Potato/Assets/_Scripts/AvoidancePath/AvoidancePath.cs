286b9e3a07416e455753ed0d27be5f2efe711330
Calvin Kopitz <calvinjk@umich.edu>
2017-03-11 17:37:50 -0500

    Added light to the Main scene and small work done on AvoidancePath

--- Puzzle Potato/Assets/_Scripts/AvoidancePath/AvoidancePath.cs
+++ Puzzle Potato/Assets/_Scripts/AvoidancePath/AvoidancePath.cs
@@ -9,8 +9,36 @@ namespace com.aaronandco.puzzlepotato {
 
         public bool ____________________________;  // Separation between public and "private" variables in the inspector
 
+        public bool inProgress = false;
+
         void Awake() {
             Initialize();
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+            if (!inProgress) {
+                if (Input.touchCount > 0) {
+                    Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                    Vector2 touchPos = new Vector2(wp.x, wp.y);
+                    Collider2D colInfo = Physics2D.OverlapPoint(touchPos);
+                    if (colInfo != null) { // StartingArea was touched
+                        SwapMode();
+                    }
+                }
+            } else { // inProgress == true
+
+            }
+        }
+
+        void SwapMode() {
+            /// TODO - 
+            /// Spawn all the moving parts and whatnot
+            /// Disable starting area
+            /// Enable ending area
         }
 
         public override void StartGame() {
