892ca76a02ee17b321a2d932122e22bdf46b8866
Calvin Kopitz <calvinjk@umich.edu>
2017-03-11 15:46:01 -0500

    Re-organized folders and started work on AvoidancePath puzzle

---
+++ Puzzle Potato/Assets/_Scripts/AvoidancePath/AvoidancePath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.aaronandco.puzzlepotato {
+    public class AvoidancePath : Puzzle {
+
+        public GameObject startingArea;
+
+        public bool ____________________________;  // Separation between public and "private" variables in the inspector
+
+        void Awake() {
+            Initialize();
+        }
+
+        public override void StartGame() {
+            Instantiate(startingArea);
+        }
+    }
+}
