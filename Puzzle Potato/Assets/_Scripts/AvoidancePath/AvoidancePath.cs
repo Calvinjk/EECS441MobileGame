766597d1d7c7ddc3ae6cc41996d49f3abd2182bf
Calvin Kopitz <calvinjk@umich.edu>
2017-03-11 22:48:16 -0500

    AvoidanceTouch nearing completion, touch not tested

--- Puzzle Potato/Assets/_Scripts/AvoidancePath/AvoidancePath.cs
+++ Puzzle Potato/Assets/_Scripts/AvoidancePath/AvoidancePath.cs
@@ -6,17 +6,20 @@ namespace com.aaronandco.puzzlepotato {
     public class AvoidancePath : Puzzle {
 
         public GameObject startingArea;
+        public GameObject fallingBlock;
+        public float spawnFrequency = 0.5f;
+        public float maxSize = 10f;
+        public float minSize = 1f;
 
         public bool ____________________________;  // Separation between public and "private" variables in the inspector
 
+        public List<GameObject> blocks;
         public bool inProgress = false;
+        public float curTime = 0;
+        float topBound = 5f;
 
         void Awake() {
             Initialize();
-        }
-
-        void Start() {
-
         }
 
         void Update() {
@@ -30,15 +33,34 @@ namespace com.aaronandco.puzzlepotato {
                     }
                 }
             } else { // inProgress == true
+                curTime -= Time.deltaTime;
+                if (curTime < 0) {
+                    curTime = spawnFrequency;
+                    float blockSize = Random.Range(minSize, maxSize);
+                    int hPos = Random.Range((int)-4, (int)5);
+                    GameObject block = Instantiate(fallingBlock, new Vector3(hPos, topBound + (blockSize / 2), 0), Quaternion.identity) as GameObject;
+                    block.transform.localScale = new Vector3(1, blockSize, 1);
 
+                    blocks.Add(block);
+                }
             }
         }
 
         void SwapMode() {
-            /// TODO - 
-            /// Spawn all the moving parts and whatnot
-            /// Disable starting area
-            /// Enable ending area
+            if (inProgress) {
+                // Delete all blocks on screen
+                foreach (GameObject obj in blocks) {
+                    Destroy(obj);
+                }
+                blocks.Clear();
+
+                // Re-spawn the starting area
+                Instantiate(startingArea);
+            } else {
+                // TODO
+                // spawn in a bunch of blocks to start (one each horizontally ?)
+                // Disable starting area
+            }
         }
 
         public override void StartGame() {
---
+++ Puzzle Potato/Assets/_Scripts/AvoidancePath/FallDown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.aaronandco.puzzlepotato {
+    public class FallDown : MonoBehaviour {
+
+        public float speed = 10f;
+        public float lifetime = 40f;
+
+        public bool ____________________________;  // Separation between public and "private" variables in the inspector
+
+        float speedMultiplier = 0.01f;
+
+        void Update() {
+            transform.position = new Vector3(transform.position.x, transform.position.y - (speed * speedMultiplier), 0);
+
+            lifetime -= Time.deltaTime;
+            if (lifetime < 0) { Destroy(this.gameObject); }
+        }
+    }
+}
