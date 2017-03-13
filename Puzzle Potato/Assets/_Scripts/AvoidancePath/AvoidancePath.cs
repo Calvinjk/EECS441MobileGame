55d4a84291311f5f13b30170b8be497033a4b270
Calvin Kopitz <calvinjk@umich.edu>
2017-03-13 12:32:01 -0400

    AvoidancePath completed

--- Puzzle Potato/Assets/_Scripts/AvoidancePath/AvoidancePath.cs
+++ Puzzle Potato/Assets/_Scripts/AvoidancePath/AvoidancePath.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 namespace com.aaronandco.puzzlepotato {
     public class AvoidancePath : Puzzle {
 
-        public GameObject startingArea;
+        public GameObject goalArea;
         public GameObject fallingBlock;
         public float spawnFrequency = 0.5f;
+        public float minSpeed = 10f;
+        public float maxSpeed = 30f;
         public float maxSize = 10f;
         public float minSize = 1f;
         public bool debugLogs = true;
@@ -16,6 +18,7 @@ namespace com.aaronandco.puzzlepotato {
 
         public List<GameObject> blocks;
         GameObject startingAreaInstance;
+        GameObject endingAreaInstance;
         public bool inProgress = false;
         public float curTime = 0;
         float topBound = 5f;
@@ -44,11 +47,26 @@ namespace com.aaronandco.puzzlepotato {
                     GameObject block = Instantiate(fallingBlock, new Vector3(hPos, topBound + (blockSize / 2), 0), Quaternion.identity) as GameObject;
                     block.transform.localScale = new Vector3(1, blockSize, 1);
 
+                    FallDown blockScript = (FallDown)block.GetComponent("FallDown");
+                    blockScript.speed = Random.Range(minSpeed, maxSpeed);
+
                     blocks.Add(block);
                 }
                 if (Input.touchCount == 0 || Input.GetTouch(0).phase == TouchPhase.Ended) {
                     if (debugLogs) { Debug.Log("Player lifted finger off screen"); }
                     SwapMode();
+                }
+
+                // Check if player won or touched a falling block
+                Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                Vector2 touchPos = new Vector2(wp.x, wp.y);
+                Collider2D colInfo = Physics2D.OverlapPoint(touchPos);
+                if (colInfo != null) {
+                    if (colInfo.gameObject.name == "AvoidanceArea(Clone)") {
+                        GameCompleted();
+                    } else { // Falling block was touched
+                        SwapMode();
+                    }
                 }
             }
         }
@@ -65,20 +83,42 @@ namespace com.aaronandco.puzzlepotato {
                 blocks.Clear();
 
                 // Re-spawn the starting area
-                startingAreaInstance = Instantiate(startingArea);
+                startingAreaInstance = Instantiate(goalArea);
+                Destroy(endingAreaInstance);
             } else {
                 // Swap mode
                 inProgress = true;
                 Destroy(startingAreaInstance);
+                endingAreaInstance = Instantiate(goalArea, new Vector3(6.25f, 0, 0), Quaternion.identity);
 
-                // TODO
-                // spawn in a bunch of blocks to start (one each horizontally ?)
-                // Disable starting area
+                //Spawn a bunch of starting blocks
+                List<int> positions = new List<int>();
+                for (int i = 0; i < 6; ++i) {                   
+                    float blockSize = Random.Range(minSize, maxSize);
+                    int hPos = Random.Range((int)-4, (int)5);
+                    for (int j = 0; j < positions.Count; ++j) {
+                        if (positions[j] == hPos) {
+                            hPos = Random.Range((int)-4, (int)5);
+                            j = 0;
+                        }
+                    }
+                    positions.Add(hPos);
+
+                    float vPos = Random.Range(-2f, 5f);
+
+                    GameObject block = Instantiate(fallingBlock, new Vector3(hPos, vPos, 0), Quaternion.identity) as GameObject;
+                    block.transform.localScale = new Vector3(1, blockSize, 1);
+
+                    FallDown blockScript = (FallDown)block.GetComponent("FallDown");
+                    blockScript.speed = Random.Range(minSpeed, maxSpeed);
+
+                    blocks.Add(block);
+                }
             }
         }
 
         public override void StartGame() {
-            startingAreaInstance = Instantiate(startingArea);
+            startingAreaInstance = Instantiate(goalArea);
         }
     }
 }
