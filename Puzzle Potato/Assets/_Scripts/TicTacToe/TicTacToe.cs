b43821bc585a9f211e9b108924cfdcc017970924
Calvin Kopitz <calvinjk@umich.edu>
2017-03-15 11:31:58 -0400

    Started work on TicTacToe puzzle

---
+++ Puzzle Potato/Assets/_Scripts/TicTacToe/TicTacToe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.aaronandco.puzzlepotato {
+    public class TicTacToe : Puzzle {
+
+        public GameObject board;
+        public Sprite xSprite;
+        public Sprite oSprite;
+
+        public bool ____________________________;  // Separation between public and "private" variables in the inspector
+
+        void Start() {
+            StartGame(); // Delete this later, this is for testing
+        }
+
+        public override void StartGame() {
+            // Create the UI for the game
+        }
+    }
+}
