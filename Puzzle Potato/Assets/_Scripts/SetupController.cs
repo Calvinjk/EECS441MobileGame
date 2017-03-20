05b071a436e78e1e08a216664e3f4284a341763c
Hannah Kim <hannah@0587395082.wireless.umich.net>
2017-03-19 21:32:27 -0400

    ui fixed, playerlistdelete not fixed :(

--- Puzzle Potato/Assets/_Scripts/SetupController.cs
+++ Puzzle Potato/Assets/_Scripts/SetupController.cs
@@ -2,81 +2,92 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace com.aaronandco.puzzlepotato {
     public class SetupController : MonoBehaviour {
         // All these must be added by hand in the inspector
+
+        public List<GameObject> allPlayers;
+        public GameObject playerList;
+
+        public bool ______________________________;
+
         public GameObject titlePanel;
         public GameObject addPlayersPanel;
         public GameObject startPanel;
-		public Text instruction;
+        public Text instruction;
         public InputField playerNameInput;
         public GameObject playerCount;
-        public GameObject playerList;
-		public GameObject readyButton;
-		public GameObject addPlayerButton;
-		public GameObject backButton;
-		public GameObject playButton;
+        public GameObject readyButton;
+        public GameObject addPlayerButton;
+        public GameObject backButton;
+        public GameObject playButton;
         public Text beginButtonText;
-
-        public bool ____________________________;  // Separation between public and "private" variables in the inspector
+        public GameObject playerButton;
+        public GameManager gameManagerScript;
 
         public List<string> playerNames = new List<string>();
-        public GameManager gameManagerScript;
+        // public List<GameObject> allPlayers = new List<GameObject>();
 
         // Make sure the players only see the title screen at first and find the GameManager script
         void Awake() {
             SetView(0);
             gameManagerScript = (GameManager)GameObject.FindGameObjectWithTag("GameManager").GetComponent("GameManager");
+
         }
 
         // Sets which view is active
         // 0 - Title
         // 1 - Add Players
-		// 2 - Confirm players/settings
+        // 2 - Confirm players/settings
         // 3 - START
         public void SetView (int panel)
-		{
-			titlePanel.SetActive (false);
-			addPlayersPanel.SetActive (false);
-			startPanel.SetActive (false);
+        {
+            titlePanel.SetActive (false);
+            addPlayersPanel.SetActive (false);
+            startPanel.SetActive (false);
 
-			playButton.SetActive (false);
-			backButton.SetActive (false);
-			addPlayerButton.SetActive (false);
-			readyButton.SetActive (false);
-			switch (panel) {
-				case 0:
-					titlePanel.SetActive (true);
-		
-	                // If we are going back to the title screen, wipe players and reset text
-					playerNames.Clear ();
-					playerCount.GetComponent<Text> ().text = "Number  of  players: 0";
-					playerList.GetComponent<Text> ().text = "Current  Players:  \n \t None";
-					playerList.GetComponent<Text> ().color = Color.black;
-					break;
-				case 1:
-					addPlayersPanel.SetActive (true);
-					instruction.text = "please  enter  between  2  and  8  players";
+            playButton.SetActive (false);
+            backButton.SetActive (false);
+            addPlayerButton.SetActive (false);
+            readyButton.SetActive (false);
 
-					readyButton.SetActive (false);
-					addPlayerButton.SetActive (true);
-					
-					break;
-				case 2:
+            switch (panel) {
+                case 0:
+                    titlePanel.SetActive (true);
+        
+                    // If we are going back to the title screen, wipe players and reset text
+                    playerNames.Clear ();
+                    playerCount.GetComponent<Text> ().text = "Number  of  players: 0";
+                    // playerList.GetComponent<Text> ().text = "Current  Players:  \n \t None";
+                    // playerList.GetComponent<Text> ().color = Color.black;
+                    break;
+                case 1:
+                    addPlayersPanel.SetActive (true);
+                    instruction.text = "please  enter  between  2  and  8  players";
+
+                    if (playerNames.Count >= 2) {
+                        readyButton.SetActive (true);
+                    }
+                    addPlayerButton.SetActive (true);
+                    
+                    break;
+                case 2:
                     addPlayersPanel.SetActive(true);
-				   	instruction.text = "is  this  correct?";
-					instruction.color = Color.black; 
+                    instruction.text = "is  this  correct?";
+                    instruction.color = Color.black; 
 
-					playButton.SetActive(true);
-					backButton.SetActive(true);
-					break;
+                    playButton.SetActive(true);
+                    backButton.SetActive(true);
+                    break;
                 case 3:
                     startPanel.SetActive(true);
 
-                    gameManagerScript.curPlayer = Random.Range(0, playerNames.Count - 1);
+                    gameManagerScript.curPlayer = UnityEngine.Random.Range(0, playerNames.Count - 1);
                     beginButtonText.text = playerNames[gameManagerScript.curPlayer] + ",  please  press  the  button  when  ready!";
                     break;
                 default:
@@ -90,12 +101,13 @@ namespace com.aaronandco.puzzlepotato {
             if (playerNameInput.text == "") {
                 return;
             }
-			if (playerNames.Count >= 1) {
-				readyButton.SetActive (true);
-			}
-			if (playerNames.Count >= 7) {
-				addPlayerButton.SetActive (false);
-			}
+            if (playerNames.Count >= 1) {
+                readyButton.SetActive (true);
+            }
+            if (playerNames.Count >= 7) {
+                addPlayerButton.SetActive (false);
+            }
+
             playerNames.Add(playerNameInput.text);
             Text playerListText = playerList.GetComponent<Text>();
 
@@ -108,7 +120,26 @@ namespace com.aaronandco.puzzlepotato {
 
             playerNameInput.text = "";
             playerListText.color = Color.black;
+
+            // playerNames.Add(playerNameInput.text);
+            // playerCount.GetComponent<Text>().text = "Number  of  players: " + playerNames.Count;
+
+            // for (int i = 0; i < playerNames.Count; ++i) {
+            //     allPlayers[i].GetComponentInChildren<Text>().text = playerNameInput.text;
+            //     allPlayers[i].SetActive(true);
+            // }
+
+            // playerNameInput.text = "";
+
         }
+
+        // // Deletes selected user ()
+        // public void DeletePlayer() {
+        //     // int indexToDelete = Int32.Parse(transform.parent.GetComponent<Text>().text);
+        //     // int indexToDelete = Int32.Parse(this.GetComponentInChildren<Text>().text);
+        //     Debug.Log(indexToDelete);
+        //     allPlayers.RemoveAt(indexToDelete);
+        // }
 
         // Write the current player list to the master GameManager and load the next scene
         public void ProceedToGame() {
