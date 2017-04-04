841b24c268cac39651b0767812ca59915dbad0a0
Calvin Kopitz <calvinjk@umich.edu>
2017-04-04 16:19:50 -0400

    Revert "Mini merge conflict with masterController"
    This reverts commit 9da2cbb83114a5ab7946867ac2b4ca9288f2cbe0, reversing
    changes made to b59e5ca90580b221b34ce7749210406c692bc499.

--- Puzzle Potato/Assets/_Scripts/Matching/Matching.cs
+++ Puzzle Potato/Assets/_Scripts/Matching/Matching.cs
@@ -6,30 +6,25 @@ using System.Collections.Generic;
 namespace com.aaronandco.puzzlepotato {
     public class Matching : Puzzle {
 
+        public List<GameObject> cards;
         public List<GameObject> cardTypes; 
 
         public bool _______________;
 
-        private Dictionary<int, Vector3> cardsPos; 
-        private Dictionary<int, GameObject> cardsFront; 
-        private Dictionary<int, GameObject> cardsBack; 
-        private int whichCard;
-        private Vector3 vector; 
+        public List<Vector3> cardsPos; 
+        public Dictionary<int, GameObject> cardsFront; 
+        public int whichCard;
 
         void Awake() {
             Initialize();
-            cardsPos = new Dictionary<int, Vector3>();
-            cardsFront = new Dictionary<int, GameObject>();
-            cardsBack = new Dictionary<int, GameObject>();
-
-            cardsPos.Add(0, new Vector3(-5.1f, 1.75f, 1));
-            cardsPos.Add(1, new Vector3(-1.6f, 1.75f, 1));
-            cardsPos.Add(2, new Vector3(1.6f, 1.75f, 1));
-            cardsPos.Add(3, new Vector3(5.1f, 1.75f, 1));
-            cardsPos.Add(4, new Vector3(-5.1f, -1.75f, 1));
-            cardsPos.Add(5, new Vector3(-1.6f, -1.75f, 1));
-            cardsPos.Add(6, new Vector3(1.6f, -1.75f, 1));
-            cardsPos.Add(7, new Vector3(5.1f, -1.75f, 1));
+            cardsPos.Add(new Vector3(-195, 75, 1));
+            cardsPos.Add(new Vector3(-65, 75, 1));
+            cardsPos.Add(new Vector3(65, 75, 1));
+            cardsPos.Add(new Vector3(195, 75, 1));
+            cardsPos.Add(new Vector3(-195, -75, 1));
+            cardsPos.Add(new Vector3(-65, -75, 1));
+            cardsPos.Add(new Vector3(65, -75, 1));
+            cardsPos.Add(new Vector3(195, -75, 1));
         }
 
         void Update() {
@@ -37,8 +32,7 @@ namespace com.aaronandco.puzzlepotato {
                 Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);    // Get the world coordinates of the screen touch
                 Vector2 touchPos = new Vector2(wp.x, wp.y);                                 // We have a 2D game, so turn the 3d coordinates into 2D (we dont care about z)
                 Collider2D colInfo = Physics2D.OverlapPoint(touchPos);                      // Determine if this 2D point is within any colliders
-                if (colInfo != null) {
-                    Debug.Log("card touched");
+                if (colInfo != null) { // If something was touched
                     return; 
                 }
             }
@@ -48,18 +42,13 @@ namespace com.aaronandco.puzzlepotato {
 
             // putting cards down
             for (int i = 0; i < 4; ++i) {
-                for (int j = 0; j < 2; ++j) {
-                    whichCard = Random.Range(0, 8);
-                    while (cardsFront.ContainsKey(whichCard)) {
-                        whichCard = Random.Range(0, 8);                    
-                    }
-                    cardsBack.Add(whichCard, Instantiate(cardTypes[4], cardsPos[whichCard], Quaternion.identity));
-                    cardsFront.Add(whichCard, Instantiate(cardTypes[i], cardsPos[whichCard], Quaternion.identity));
-                    cardsBack[whichCard].SetActive(true);
-                    cardsFront[whichCard].SetActive(true);
-                }
-            }
+                whichCard = Random.Range(0, cards.Count);
+                // do { whichCard = Random.Range(0, cards.Count);
+                // } while (!cardsFront.ContainsKey(whichCard));
 
+                // cardsFront[whichCard] = Instantiate(cardTypes[i], cardsPos[whichCard], Quaternion.identity);
+                // cardsFront[whichCard].SetActive(true);
+            }
         }
 
 
