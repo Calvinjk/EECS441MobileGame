46e22c4c37fcf328179cdb5cea40b12db843487f
Calvin Kopitz <calvinjk@umich.edu>
2017-04-12 07:33:03 -0400

    Minor logo changes

---
+++ Puzzle Potato/Assets/_Scripts/TiltMaze/RollingBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBall : MonoBehaviour {
+
+    public float speed = .1f;
+
+    public bool ____________________________;  // Separation between public and "private" variables in the inspector
+
+    float vBound = 2.5f;
+    float hBound = 7f;
+
+    // Use this for initialization
+    void Start () {
+        // Change direction of gravity to induce rolling
+        Physics.gravity = new Vector3(0f, 0f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move Ball
+        //transform.position = new Vector3(transform.position.x + Input.acceleration.x * speed, transform.position.y + Input.acceleration.y * speed, 0f);
+        GetComponent<Rigidbody>().AddForce(new Vector3(Input.acceleration.x * speed, Input.acceleration.y * speed, 0));
+
+        // Check bounds
+        //if (transform.position.y > vBound) { transform.position = new Vector3(transform.position.x, vBound, 0); }
+        //if (transform.position.y < -vBound) { transform.position = new Vector3(transform.position.x, -vBound, 0); }
+        //if (transform.position.x > hBound) { transform.position = new Vector3(hBound, transform.position.y, 0); }
+        //if (transform.position.x < -hBound) { transform.position = new Vector3(-hBound, transform.position.y, 0); }
+
+    }
+}
---
+++ Puzzle Potato/Assets/_Scripts/TiltMaze/TiltMaze.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.aaronandco.puzzlepotato {
+    public class TiltMaze : Puzzle {
+
+        public GameObject ballPrefab;
+        public GameObject boundariesPrefab;
+
+        public bool ____________________________;  // Separation between public and "private" variables in the inspector
+
+        void Start() {
+            Instantiate(ballPrefab, new Vector3(-6.5f, 2f, 0f), Quaternion.identity);
+            Instantiate(boundariesPrefab);
+        }
+
+        public override void StartGame() {
+            // TODO -- Move all awake and start shit here.
+        }
+    }
+}
